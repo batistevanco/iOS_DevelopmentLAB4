5666ec1d0c4c88f31f25397304d9a332e796cc77
Batiste Vancoillie <batiste.vancoillie@student.vives.be>
2025-10-22 10:38:02 +0200

    afgewerkt qatar

--- WK_App/WK_App/Views/ContentView.swift
+++ WK_App/WK_App/Views/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
                         )
                     }
                     .buttonStyle(.borderedProminent)
+                    .padding(.bottom, 40)
                 }
             }
             .navigationTitle("Welcome")
